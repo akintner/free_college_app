8ca0fa540917591f37d766c1c253e4aad4ea0883
Amz K <akintner@u.rochester.edu>
2017-03-01 10:11:06 -0700

    update daad university API model

--- app/models/german_university.rb
+++ app/models/german_university.rb
@@ -1,10 +1,10 @@
-class GermanUniversity < ActiveRecord::Base
-  attr_reader :id, :name, :uni_name, :city, :degree_type, :semester, :language
+class GermanUniversity 
+    attr_reader :course_name, :uni_name, :city_name, :degree_type, :semester, :language
 
     def initialize(attributes)
-      @course_name             = attributes[:name]
+      @course_name      = attributes[:name]
       @uni_name         = attributes[:hsname]
-      @city             = attributes[:hsStandort]
+      @city_name        = attributes[:hsStandort]
       @degree_type      = attributes[:abschlussart]
       @semester         = attributes[:zulassungssemester]
       @language         = attributes[:hauptsprache]
@@ -12,8 +12,8 @@ class GermanUniversity < ActiveRecord::Base
 
     def self.find_universities
       UniversityService.find_universities.map do |uni|
-        univ = University.new(name: "#{uni[:name]} at #{uni[:hsname]}", city: uni[:hsStandort], country_id: Country.find_by(name: "Germany"), language: uni[:hauptsprache], visa: "required for stays longer than 3 months")
-        univ.save
+        new(uni)
+        # University.new(name: "#{uni[:name]} at #{uni[:hsname]}", city: uni[:hsStandort], country_id: Country.find_by(name: "Germany"), language: uni[:hauptsprache], visa: "required for stays longer than 3 months")
       end
     end
   end
